729b395978bb4308227ed1e666124f4b3852a3b5
alokdev08-create <alokjavadev08@gmail.com>
2025-03-22 13:49:23 +0530

    First commit

---
+++ src/main/java/com/local/service/provider/community/support/service/CommunitySupportServiceApplication.java
@@ -0,0 +1,13 @@
+package com.local.service.provider.community.support.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunitySupportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunitySupportServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/local/service/provider/community/support/service/CommunitySupportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.local.service.provider.community.support.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunitySupportServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
